ee2b39d970655e09f00c1465817c14f587ef5ea1
Pink_Poma <choi82354@gmail.com>
2023-04-01 02:56:29 +0900

    fixed dash

--- Assets/1.Scripts/PlayerController.cs
+++ Assets/1.Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using UnityEngine.UI;
@@ -9,8 +10,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpSpeed;
+    [SerializeField] private float dashSpeed;
     [SerializeField] private bool isJump;
+    [SerializeField] private bool isWall;
+    [SerializeField] private bool isWallDash;
     [SerializeField] private PhysicsMaterial2D Friction0;
+
+    private int direction;
 
     //---- 색 게이지 관련 변수
     public Image ColorGageBar;
@@ -31,8 +37,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private SpriteRenderer _sprite;
     private Vector2 velocity = Vector2.zero;
+    private Vector2 wallDashPos = Vector2.zero;
     private float m_FrictionTime = 0.0f;    //벽면에 얼마나 붙어 있었는지 시간값을 저장할 변수
-    private int m_JumpCount = 0;
+   [SerializeField] private int m_JumpCount = 0;
 
     private float h;
 
@@ -59,7 +66,7 @@ public class PlayerController : MonoBehaviour
 
         if (m_ColorCurTime <= 0.0f)
         {
-            ColorChange();
+         //   ColorChange();
         }
 
         Move();
@@ -119,18 +126,49 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !isJump)
+        if (Input.GetKeyDown(KeyCode.Space) && !isJump && !isWall) 
         {
+            Debug.Log("점프");
             m_JumpCount++;
             _rigidbody.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
             _rigidbody.sharedMaterial = Friction0;
 
+            
+
             if(2 <= m_JumpCount)
                 isJump = true;
         }
+        else if (Input.GetKeyDown(KeyCode.Space) && isWall)
+        {
+            Debug.Log("대쉬");
+            if (!isWall) return;
+            Debug.Log(direction);
+            h = Input.GetAxis("Horizontal");
+            wallDashPos.x = -dir * (moveSpeed / 2);
+            wallDashPos.y = 1.0f * jumpSpeed;
+            wallDashPos = wallDashPos.normalized * dashSpeed;
+    
+            
+            StartCoroutine(WallDash());
+
+        }
     }
-    private void Move()
+
+    private IEnumerator WallDash()
     {
+        isWallDash = true;
+        //_rigidbody.AddForce(Vector2.right * -h * jumpSpeed, ForceMode2D.Impulse);
+       // _rigidbody.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        _rigidbody.velocity = wallDashPos;
+        _rigidbody.sharedMaterial = Friction0;
+        yield return new WaitForSeconds(0.75f);   
+        isWallDash = false;
+        isJump = true;
+    }
+    
+    private void Move()
+    {                                                                                                                                                                                                                   
+        if (isWallDash) return;
         h = Input.GetAxis("Horizontal");
 
         //바닥에 매몰되어 있을 경우(오브젝트 색 변경시 껴서 땅 아래 위치에 놓여질 경우)
@@ -177,6 +215,25 @@ public class PlayerController : MonoBehaviour
         if (col.transform.CompareTag("Enemy"))
         {
             Destroy(gameObject);
+        }
+
+        if (col.transform.CompareTag("Wall"))
+        {
+            //direction = (int)h;
+            dir = -col.contacts[0].normal.x;
+            //m_JumpCount = 3;
+            //direction = col.contacts[0].point - (Vector2)transform.position;
+            //direction = direction.normalized;
+            isWall = true;
+        }
+    }
+
+    public float dir;
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("Wall"))
+        {
+            isWall = false;
         }
     }
 
